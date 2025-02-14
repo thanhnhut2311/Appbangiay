de89252a6432597543afb9d73087533e3495a9e4
DoanVinhThanh <doanvinhthanhcc@gmail.com>
2025-02-14 14:44:31 +0700

    update

---
+++ app/src/main/java/com/example/appbangiay/SignUpActivity.java
@@ -0,0 +1,46 @@
+package com.example.appbangiay;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class SignUpActivity extends AppCompatActivity {
+    private TextInputEditText etBirthDate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_sign_up);
+        etBirthDate = findViewById(R.id.et_birth_date);
+
+        etBirthDate.setOnClickListener(view -> showDatePicker());
+    }
+
+    private void showDatePicker() {
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                (view, year, month, dayOfMonth) -> {
+                    Calendar selectedDate = Calendar.getInstance();
+                    selectedDate.set(year, month, dayOfMonth);
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+                    etBirthDate.setText(sdf.format(selectedDate.getTime()));
+                },
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+}
